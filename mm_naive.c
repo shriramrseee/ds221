8c730679f6a3ebe3200cc38fbdc826e07d0fe4ac
Shriram Ramesh <shriramrseee@gmail.com>
2018-08-29 09:51:38 +0000

    Add Cache and Naive version

---
+++ mm_naive.c
@@ -0,0 +1,31 @@
+#include <stdlib.h>
+
+#define SIZE 4096
+
+double a[SIZE][SIZE], b[SIZE][SIZE], c[SIZE][SIZE];
+
+int main()
+{
+	for(int i=0; i<SIZE; i++)
+	{
+		for(int j=0; j<SIZE; j++)
+		{
+			a[i][j]=rand();
+			b[i][j]=rand();
+			c[i][j]=0;
+		}
+	}
+
+	for(int i=0; i<SIZE; i++)
+	{
+		for(int j=0; j<SIZE; j++)
+		{
+			for(int k=0; k<SIZE; k++)
+			{
+				c[i][j] += a[i][k]*b[k][j];
+			}
+		}
+	}
+
+	return 0;
+}
