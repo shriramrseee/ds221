0e2b9243dba71ce1ea9ca766286c835cd8f08095
Shriram Ramesh <shriramrseee@gmail.com>
2018-09-25 17:55:07 +0530

    Format and add comments

--- part_2/code/MatrixImpl.cpp
+++ part_2/code/MatrixImpl.cpp
@@ -1,250 +1,218 @@
+/**
+ * Created by: Shriram R.
+ * Roll No: 06-02-01-10-51-18-1-15763
+ */
+
 #include"IMatrix.h"
 #include<iostream>
 
 using namespace std;
 
 
-/**
- * 2D Matrix Implementation usign Arrays
- */
-class ArrayMatrix: public IMatrix
-{  
+// Matrix implementation  using arrays
+class ArrayMatrix : public IMatrix {
 
-	private:
-		bool initialized;
-		int rows;
-		int cols;
-		int curr_row;
-                int nnz; 
-		float** mat;
-   
-	public:
-		
-   
-	ArrayMatrix()
-	{ 
-		initialized = 0;
-		rows = 0;
-		cols = 0;
-		curr_row = 0;
-                nnz = 0;
-	}
- 
+private:
+    bool initialized; // Indicates if the matrix is initialized (1: initialized and 0: not initialized)
+    int rows; // Row size
+    int cols; // Column size
+    int curr_row; // Stores the index of latest row that has been appended
+    int nnz; // Stores the no. of non-zero elements
+    float **mat; // pointer to the 2D array
 
-	/** Override the init function defined in the IMatrix interface */
-	void init (int N, int M)
- 	{ 
-	    if(initialized) {
-                throw logic_error(string("Matrix already initialized!"));
-            }
-            if (N<=0 || M<=0) {
-                throw out_of_range(string("Row/Column size is not strictly positive!"));
-            }
-            mat = new float*[N];
-            for(int i=0; i<N; i++) {
-                mat[i] = new float[M];
-            }
-            for(int i=0; i<N; i++) {
-                for(int j=0; j<M; j++) {
-                    mat[i][j] = 0;
-                }
-            }
-            initialized = 1;
-            rows=N;
-            cols=M;
-	}
+public:
 
-	
-	/** Override the append function defined in the IMatrix interface */
-   	void append (float* row_vals)
-	{ 
-	    if(curr_row == rows) {
-                throw out_of_range(string("Cannot append row with a different column size!"));
-            }
-            for(int i=0; i<cols; i++) {
-                mat[curr_row][i] = row_vals[i];
-                if(row_vals[i]!=0)
-		    nnz++;
-            }
-            curr_row++;
-	}
+    // Constructor
+    ArrayMatrix() {
+        initialized = 0;
+        rows = 0;
+        cols = 0;
+        curr_row = 0;
+        nnz = 0;
+    }
 
-	
-	/** Override the get function defined in the IMatrix interface */
-	float get(int i, int j)
-	{ 
-	    if(i >= rows || j >= cols) {
-                throw out_of_range(string("Row or Column index is out of range!"));
+    // Initializes the 2D arrray
+    void init(int N, int M) {
+        if (initialized) {
+            throw logic_error(string("Matrix already initialized!"));
+        }
+        // If N or M <= 0 (Invalid size), throw an exception
+        if (N <= 0 || M <= 0) {
+            throw logic_error(string("Row/Column size is not strictly positive!"));
+        }
+        mat = new float *[N];
+        for (int i = 0; i < N; i++) {
+            mat[i] = new float[M];
+        }
+        // Initialize the matrix with 0
+        for (int i = 0; i < N; i++) {
+            for (int j = 0; j < M; j++) {
+                mat[i][j] = 0;
             }
-            return mat[i][j];
-	}
+        }
+        initialized = 1;
+        rows = N;
+        cols = M;
+    }
 
-	
-	/**
-	  * This returns the number of rows in the matrix based on init()
-	  */
-	int row_count()
-	{
-	    return rows;
-	}
-	
-	/**
-	  * This returns the number of columns in the matrix based on init()
-	  */
-	int col_count() 
-	{
-	    return cols;
-	}
+    // Appends the input row to the existing matrix
+    void append(float *row_vals) {
+        // Throws an exception if the matrix is full
+        if (curr_row == rows) {
+            throw out_of_range(string("Cannot append more rows!"));
+        }
+        for (int i = 0; i < cols; i++) {
+            mat[curr_row][i] = row_vals[i];
+            if (row_vals[i] != 0)
+                nnz++; // Increment for each non-zero element
+        }
+        curr_row++;
+    }
 
-	
-	/**
-	  * This returns the number of non zero values in the matrix
-	  */
-	int nnz_count()
-	{
-	    return nnz++;
-	}
+    // Returns the value of a particular entry in the matrix
+    float get(int i, int j) {
+        // Throws an exception if the index is out of range
+        if (i >= rows || j >= cols) {
+            throw out_of_range(string("Row or Column index is out of range!"));
+        }
+        return mat[i][j];
+    }
+
+    // Returns the number of rows in the matrix
+    int row_count() {
+        return rows;
+    }
+
+    // Returns the number of columns in the matrix
+    int col_count() {
+        return cols;
+    }
+
+    // Returns the number of non zero values in the matrix
+    int nnz_count() {
+        return nnz;
+    }
 
 };
 
 
+// Matrix Implementation using CSR
+class CSRMatrix : public IMatrix {
 
-/**
- * 2D Matrix Implementation usign CSR
- */
-class CSRMatrix: public IMatrix
-{  	
+private:
+    bool initialized; // Indicates if the matrix is initialized (1: initialized and 0: not initialized)
+    int rows; // Row size
+    int cols; // Column size
+    int curr_row; // Stores the index of latest row that has been appended
+    int nnz; // Stores the no. of non-zero elements
+    float *a; // Pointer to the array storing the non-zero elements
+    int *ia; // Pointer to the array storing the cumulative count of nnz values in terms of rows
+    int *ja; // Pointer to the array storing the column index of nnz elements
+    int capacity; // Max. capacity of non-zero elements
 
-	private: 
-		bool initialized;
-		int rows;
-		int cols;
-		int curr_row;
-		int nnz;
-		float* a;
-		int* ia;
-		int* ja;
-		int capacity;
-   
-	public:
-   
-   
-	CSRMatrix()
-	{ 
-		initialized = 0;
-		rows = 0;
-		cols = 0;
-		curr_row = 0;
-                nnz = 0;
-	}
- 
+public:
 
-	/** Override the init function defined in the IMatrix interface */
-	void init (int N, int M)
- 	{ 
-	    if(initialized) {
-                throw logic_error(string("Matrix already initialized!"));
-            }
-            if (N<=0 || M<=0) {
-                throw out_of_range(string("Row/Column size is not strictly positive!"));
-            }
-            a = new float[2];
-            ia = new int[N+1];
-            ja = new int[2];
-            for(int i=0; i<N+1; i++) {
-                ia[i] = 0;
-            }
-            initialized = 1;
-            rows=N;
-            cols=M;
-            curr_row = 0;
-            capacity = 2;
-	}
-	
+    // Constructor
+    CSRMatrix() {
+        initialized = 0;
+        rows = 0;
+        cols = 0;
+        curr_row = 0;
+        nnz = 0;
+    }
 
-	/** Override the append function defined in the IMatrix interface */
-   	void append (float* row_vals)
-	{ 
-	    if(curr_row == rows) {
-                throw out_of_range(string("Cannot append row with a different column size!"));
-            }
-            curr_row++;
-            for(int i=0; i<cols; i++) {
-                if(row_vals[i] != 0)
-                {
-		        if(ia[curr_row] >= capacity) {
-		            extend();
-		        }
-		        ja[ia[curr_row]] = i;
-		        a[ia[curr_row]] = row_vals[i];
-		        ia[curr_row]++;
-                        nnz++;
-                }
-            }
-            for(int i=curr_row+1; i<rows+1; i++) {
-                ia[i] = ia[i-1];
-            }
-	}
-
-	
-	/** Override the get function defined in the IMatrix interface */
-	float get(int i, int j)
-	{ 
-	    if(i >= rows || j >= cols) {
-                throw out_of_range(string("Row or Column index is out of range!"));
-            }
-            float val = 0;
-            for(int x=ia[i]; x<ia[i+1]; x++) {
-                if(ja[x]==j) {
-                    val = a[x];
-                    break;
-                }
-            }
-            return val;
-	}
-
-	/**
-	  * This returns the number of rows in the matrix based on init()
-	  */
-	int row_count()
-	{
-	    return rows;	
-	}
-	
-	/**
-	  * This returns the number of columns in the matrix based on init()
-	  */
-	int col_count() 
-	{
-	    return cols;
-	}
-
-	
-	/**
-	  * This returns the number of non zero values in the matrix
-	  */
-	int nnz_count()
-	{
-	    return nnz;	
-	}
-
-
-        void extend() {
-            float* ta;
-            int* tja;
-            ta = a;
-            tja = ja;
-            a = new float[capacity*2];
-            ja = new int[capacity*2];
-            for(int i=0; i<capacity; i++) {
-                a[i] = ta[i];
-                ja[i] = tja[i];
-            }
-            capacity = capacity*2;
-            delete ta;
-            delete tja;
+    // Initializes the 2D arrray
+    void init(int N, int M) {
+        if (initialized) {
+            throw logic_error(string("Matrix already initialized!"));
         }
+        // If N or M <= 0 (Invalid size), throw an exception
+        if (N <= 0 || M <= 0) {
+            throw out_of_range(string("Row/Column size is not strictly positive!"));
+        }
+        a = new float[2]; // Initial capacity is 2
+        ia = new int[N + 1]; // Size is rows + 1
+        ja = new int[2]; // Size same as value array 'a'
+        for (int i = 0; i < N + 1; i++) {
+            ia[i] = 0; // No nnz elements initially
+        }
+        initialized = 1;
+        rows = N;
+        cols = M;
+        curr_row = 0;
+        capacity = 2; // Current capacity
+    }
+
+    // Appends the input row to the existing matrix
+    void append(float *row_vals) {
+        // Throws an exception if the matrix is full
+        if (curr_row == rows) {
+            throw out_of_range(string("Cannot append row with a different column size!"));
+        }
+        curr_row++;
+        for (int i = 0; i < cols; i++) {
+            if (row_vals[i] != 0) { // Execute only if the value is nnz
+                if (ia[curr_row] >= capacity) {
+                    extend(); // Extend the array if capacity is reached
+                }
+                ja[ia[curr_row]] = i; // Store the column index
+                a[ia[curr_row]] = row_vals[i]; // Store the actual nnz value
+                ia[curr_row]++; // Increment the cumulative counter
+                nnz++; // Increment the nnz counter
+            }
+        }
+        for (int i = curr_row + 1; i < rows + 1; i++) {
+            ia[i] = ia[i - 1]; // Increment the cumulative counter
+        }
+    }
+
+    // Returns the value of a particular entry in the matrix
+    float get(int i, int j) {
+        // Throws an exception if the index is out of range
+        if (i >= rows || j >= cols) {
+            throw out_of_range(string("Row or Column index is out of range!"));
+        }
+        float val = 0;
+        for (int x = ia[i]; x < ia[i + 1]; x++) { // Use the cumulative nnz counter to identify the starting position
+            if (ja[x] == j) { // If column index is equal
+                val = a[x];
+                break;
+            }
+        }
+        return val;
+    }
+
+    // Returns the number of rows in the matrix
+    int row_count() {
+        return rows;
+    }
+
+    // Returns the number of columns in the matrix
+    int col_count() {
+        return cols;
+    }
+
+    // Returns the number of non zero values in the matrix
+    int nnz_count() {
+        return nnz;
+    }
+
+    // Extends the arrrays by a factor of 2
+    void extend() {
+        float *ta;
+        int *tja;
+        ta = a;
+        tja = ja;
+        a = new float[capacity * 2]; // New array with double capacity
+        ja = new int[capacity * 2]; // New array with double capacity
+        for (int i = 0; i < capacity; i++) { // Copy the values from old array to the new array
+            a[i] = ta[i];
+            ja[i] = tja[i];
+        }
+        capacity = capacity * 2; // Update the capacity
+        delete ta; // Delete old array
+        delete tja; // Delete old array
+    }
 
 
-	
 };
--- part_2/code/Runner.cpp
+++ part_2/code/Runner.cpp
@@ -1,3 +1,8 @@
+/**
+ * Created by: Shriram R.
+ * Roll No: 06-02-01-10-51-18-1-15763
+ */
+
 #include <iostream>
 #include <string.h>
 #include <cstdlib>
@@ -10,302 +15,273 @@
 
 using namespace std;
 
-IMatrix* load_arr(char* input_file) 
-{
+// Load an input file into an array based matrix
+IMatrix *load_arr(char *input_file) {
     ifstream input;
-    IMatrix* mat = new ArrayMatrix;
+    IMatrix *mat = new ArrayMatrix;
     int N, M;
-
+    // Open the file
     input.open(input_file);
-    if(!input.is_open())
-    {
-     throw invalid_argument(string("Input file does not exist!"));
+    if (!input.is_open()) {
+        throw invalid_argument(string("Input file does not exist!"));
     }
-
-    input>>N>>M;
+    // Load data into the matrix
+    input >> N >> M;
     mat->init(N, M);
-    float* row = new float[M];
-    for(int i=0; i<N; i++) {
-	for(int j=0; j<M; j++) {
-	    input>>row[j];
-	}
-	mat->append(row);
+    float *row = new float[M];
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            input >> row[j];
+        }
+        mat->append(row);
     }
-
-    input.close();    
+    // Close the file
+    input.close();
     return mat;
 
 }
 
-IMatrix* load_csr(char* input_file) 
-{
+// Load an input file into an csr based matrix
+IMatrix *load_csr(char *input_file) {
     ifstream input;
-    IMatrix* mat = new CSRMatrix;
+    IMatrix *mat = new CSRMatrix;
     int N, M;
-
+    // Open the file
     input.open(input_file);
-    if(!input.is_open())
-    {
-     throw invalid_argument(string("Input file does not exist!"));
+    if (!input.is_open()) {
+        throw invalid_argument(string("Input file does not exist!"));
     }
-
-    input>>N>>M;
+    // Load data into the matrix
+    input >> N >> M;
     mat->init(N, M);
-    float* row = new float[M];
-
-    for(int i=0; i<N; i++) {
-	for(int j=0; j<M; j++) {
-	    input>>row[j];
-	}
-	mat->append(row);
+    float *row = new float[M];
+    for (int i = 0; i < N; i++) {
+        for (int j = 0; j < M; j++) {
+            input >> row[j];
+        }
+        mat->append(row);
     }
-    
+    // Close the file
     input.close();
     return mat;
 }
 
-IMatrix* init_arr(int rows, int cols) 
-{
-    IMatrix* mat = new ArrayMatrix;
+// Initialize an array based matrix
+IMatrix *init_arr(int rows, int cols) {
+    IMatrix *mat = new ArrayMatrix;
     mat->init(rows, cols);
     return mat;
 }
 
-
-IMatrix* init_csr(int rows, int cols) 
-{
-    IMatrix* mat = new CSRMatrix;
+// Initialize an csr based matrix
+IMatrix *init_csr(int rows, int cols) {
+    IMatrix *mat = new CSRMatrix;
     mat->init(rows, cols);
     return mat;
 }
 
-
-void print_mat(IMatrix* mat, char* output_file) 
-{
+// Write a matrix to the specified file
+void print_mat(IMatrix *mat, char *output_file) {
     ofstream output;
     int rows, cols;
     rows = mat->row_count();
     cols = mat->col_count();
-
+    // Open the file
     output.open(output_file);
-
-    output<<rows<<"\t"<<cols<<"\t\n";
-    for(int i=0; i<rows; i++)
-    {
-         for(int j=0; j<cols; j++)
-         {
-             output<<setprecision(3)<<mat->get(i, j)<<"\t";
-         }
-         output<<"\n";
+    // Write the data to the file
+    output << rows << "\t" << cols << "\t\n";
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            output << setprecision(3) << mat->get(i, j) << "\t";
+        }
+        output << "\n";
     }
-
-    output.close();    
+    // Close the file
+    output.close();
 }
 
-void load(char* mat_type, char* input_file, char* output_file)
-{
-	
-    IMatrix* mat1;
+// Loads a matrix from an input file and writes it back to an output file
+void load(char *mat_type, char *input_file, char *output_file) {
+
+    IMatrix *mat1;
     struct timespec start, end;
 
-    if (strcmp("array", mat_type)==0) 
-    { 
-	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);
+    if (strcmp("array", mat_type) == 0) {
+        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start); // Start timer
         mat1 = load_arr(input_file);
-        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);
-    }  
-    else if (strcmp("csr", mat_type)==0)
-    {  
-	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);
+        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end); // End timer
+    } else if (strcmp("csr", mat_type) == 0) {
+        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start); // Start timer
         mat1 = load_csr(input_file);
-        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);
-    }
-    else
-	cout<<"[load] invalid matrix type input seen: "<<mat_type<<endl;
+        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end); // End Timer
+    } else
+        cout << "[load] invalid matrix type input seen: " << mat_type << endl;
 
     print_mat(mat1, output_file);
-
-    cout<<"load,"<<mat_type<<","<<output_file<<","<<(end.tv_nsec-start.tv_nsec)/1000000.0 + (end.tv_sec-start.tv_sec)*1000<<"\n";
+    // Print time taken in milliseconds to stdout
+    cout << "load," << mat_type << "," << output_file << ","
+         << (end.tv_nsec - start.tv_nsec) / 1000000.0 + (end.tv_sec - start.tv_sec) * 1000 << "\n";
     return;
 }
 
-
-void add_mat(IMatrix* mat1, IMatrix* mat2, IMatrix* mat3) 
-{	
+// Adds two matrices and stores the result in a result matrix
+void add_mat(IMatrix *mat1, IMatrix *mat2, IMatrix *mat3) {
     int rows, cols;
     rows = mat1->row_count();
     cols = mat1->col_count();
-    float* row = new float[cols];
-    for(int i=0; i<rows; i++) {
-	for(int j=0; j<cols; j++) {
-	    row[j] = mat1->get(i, j) + mat2->get(i, j);
-	}
-	mat3->append(row);
+
+    float *row = new float[cols];
+    for (int i = 0; i < rows; i++) {
+        for (int j = 0; j < cols; j++) {
+            row[j] = mat1->get(i, j) + mat2->get(i, j);
+        }
+        mat3->append(row);
     }
 }
 
+// Adds two matrices from two input files and writes the result to an output file
+void add(char *mat_type, char *input_file1, char *input_file2, char *output_file) {
+    IMatrix *mat1;
+    IMatrix *mat2;
+    IMatrix *mat3;
+    struct timespec start, end;
 
-void add(char* mat_type, char* input_file1, char* input_file2, char* output_file)
-{ 
-	IMatrix* mat1;
-        IMatrix* mat2;
-        IMatrix* mat3;
-	struct timespec start, end;
-	
-	if (strcmp("array", mat_type)==0) 
-        { 
-		mat1 = load_arr(input_file1);
-		mat2 = load_arr(input_file2);
-                if(mat1->row_count() != mat2->row_count() || mat1->col_count() != mat2->col_count())
-                {
-                  throw logic_error(string("Cannot add two matrices of different dimensions!"));
-                }		
-		mat3 = init_arr(mat1->row_count(), mat1->col_count());
-        }  
-	else if (strcmp("csr", mat_type)==0)
-	{
-		mat1 = load_csr(input_file1);
-		mat2 = load_csr(input_file2);
-                if(mat1->row_count() != mat2->row_count() || mat1->col_count() != mat2->col_count())
-                {
-                  throw logic_error(string("Cannot add two matrices of different dimensions!"));
-                }		
-		mat3 = init_csr(mat1->row_count(), mat1->col_count());
-	}
-	else {
-		cout<<"[add] invalid matrix type input seen: "<<mat_type<<endl;
-		return;
-	}
-	
-	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);
-        add_mat(mat1, mat2, mat3);
-        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);
-	
-	print_mat(mat3, output_file);
-        cout<<"add,"<<mat_type<<","<<output_file<<","<<(end.tv_nsec-start.tv_nsec)/1000000.0 + (end.tv_sec-start.tv_sec)*1000<<"\n";
+    if (strcmp("array", mat_type) == 0) {
+        mat1 = load_arr(input_file1);
+        mat2 = load_arr(input_file2);
+        // If the matrices are incompatible i.e. row size or column size of the two matrices differ, throw an exception
+        if (mat1->row_count() != mat2->row_count() || mat1->col_count() != mat2->col_count()) {
+            throw logic_error(string("Cannot add two matrices of different dimensions!"));
+        }
+        mat3 = init_arr(mat1->row_count(), mat1->col_count());
+    } else if (strcmp("csr", mat_type) == 0) {
+        mat1 = load_csr(input_file1);
+        mat2 = load_csr(input_file2);
+        // If the matrices are incompatible i.e. row size or column size of the two matrices differ, throw an exception
+        if (mat1->row_count() != mat2->row_count() || mat1->col_count() != mat2->col_count()) {
+            throw logic_error(string("Cannot add two matrices of different dimensions!"));
+        }
+        mat3 = init_csr(mat1->row_count(), mat1->col_count());
+    } else {
+        cout << "[add] invalid matrix type input seen: " << mat_type << endl;
+        return;
+    }
 
-	return;
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start); // Start timer
+    add_mat(mat1, mat2, mat3);
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end); // End timer
+
+    print_mat(mat3, output_file);
+    // Print time taken in milliseconds to stdout
+    cout << "add," << mat_type << "," << output_file << ","
+         << (end.tv_nsec - start.tv_nsec) / 1000000.0 + (end.tv_sec - start.tv_sec) * 1000 << "\n";
+
+    return;
 }
 
-
-void multiply_mat(IMatrix* mat1, IMatrix* mat2, IMatrix* mat3) 
-{	
+// Multiplies two matrices and stores the result in a result matrix
+void multiply_mat(IMatrix *mat1, IMatrix *mat2, IMatrix *mat3) {
     int rows1, cols, rows2;
     rows1 = mat1->row_count();
     cols = mat1->col_count();
     rows2 = mat2->col_count();
 
-    float* row = new float[rows2];
-
-    for(int i=0; i<rows1; i++) {
-        for(int l=0; l<rows2; l++) {
-          row[l] = 0;
+    float *row = new float[rows2];
+    for (int i = 0; i < rows1; i++) { // Iterate over rows of result matrix
+        for (int l = 0; l < rows2; l++) { // Initialize the row vector to zero
+            row[l] = 0;
         }
-	for(int j=0; j<cols; j++) {
-          for(int k=0; k<rows2; k++) {
-	    row[k] += mat1->get(i, j)*mat2->get(j, k);
-	  }
+        for (int j = 0; j < cols; j++) { // Iterate over columns of first matrix
+            for (int k = 0; k < rows2; k++) { // Iterate over columns of result matrix
+                row[k] += mat1->get(i, j) * mat2->get(j, k);
+            }
         }
-	mat3->append(row);
+        mat3->append(row);
     }
 }
 
+// Multiplies two matrices from two input files and writes the result to an output file
+void multiply(char *mat_type, char *input_file1, char *input_file2, char *output_file) {
+    IMatrix *mat1;
+    IMatrix *mat2;
+    IMatrix *mat3;
+    struct timespec start, end;
 
+    if (strcmp("array", mat_type) == 0) {
+        mat1 = load_arr(input_file1);
+        mat2 = load_arr(input_file2);
+        // If the matrices are incompatible i.e. column size of first and row size of second matrices differ, throw an exception
+        if (mat1->col_count() != mat2->row_count()) {
+            throw logic_error(string("Input matrices are incompatible for multiplication!"));
+        }
+        mat3 = init_arr(mat1->row_count(), mat2->col_count());
 
-void multiply(char* mat_type, char* input_file1, char* input_file2, char* output_file)
-{
-	IMatrix* mat1;
-        IMatrix* mat2;
-        IMatrix* mat3;
-        struct timespec start, end;
+    } else if (strcmp("csr", mat_type) == 0) {
+        mat1 = load_csr(input_file1);
+        mat2 = load_csr(input_file2);
+        // If the matrices are incompatible i.e. column size of first and row size of second matrices differ, throw an exception
+        if (mat1->col_count() != mat2->row_count()) {
+            throw logic_error(string("Input matrices are incompatible for multiplication!"));
+        }
+        mat3 = init_csr(mat1->row_count(), mat2->col_count());
 
-	if (strcmp("array", mat_type)==0) 
-        { 
-		mat1 = load_arr(input_file1);
-		mat2 = load_arr(input_file2);
-                if(mat1->col_count() != mat2->row_count())
-                {
-                  throw logic_error(string("Input matrices are incompatible for multiplication!"));
-                }
-		mat3 = init_arr(mat1->row_count(), mat2->col_count());
-		
-	}  
-	else if (strcmp("csr", mat_type)==0)
-	{
-		mat1 = load_csr(input_file1);
-		mat2 = load_csr(input_file2);
-                if(mat1->col_count() != mat2->row_count())
-                {
-                  throw logic_error(string("Input matrices are incompatible for multiplication!"));
-                }
-		mat3 = init_csr(mat1->row_count(), mat2->col_count());
-		
-	}
-	else {
-		cout<<"[multiply] invalid matrix type input seen: "<<mat_type<<endl;
-		return;
-	}
-	
-	clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start);
-	multiply_mat(mat1, mat2, mat3);
-        clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end);
+    } else {
+        cout << "[multiply] invalid matrix type input seen: " << mat_type << endl;
+        return;
+    }
 
-	print_mat(mat3, output_file);
-        cout<<"multiply,"<<mat_type<<","<<output_file<<","<<(end.tv_nsec-start.tv_nsec)/1000000.0 + (end.tv_sec-start.tv_sec)*1000<<"\n";
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start); // Start timer
+    multiply_mat(mat1, mat2, mat3);
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end); // End timer
 
-	return;
+    print_mat(mat3, output_file);
+    // Print time taken in milliseconds to stdout
+    cout << "multiply," << mat_type << "," << output_file << ","
+         << (end.tv_nsec - start.tv_nsec) / 1000000.0 + (end.tv_sec - start.tv_sec) * 1000 << "\n";
+
+    return;
 }
 
 
-void bfs(char* input_file, char* root_id, char* output_file)
-{
- 
-	// TODO: any validation?
-	
-	IMatrix* mat1;
-	// TODO: Define a List ADT traverse_list to store output.
+void bfs(char *input_file, char *root_id, char *output_file) {
 
-	// TODO
-	mat1 = load_csr(input_file);
+    // TODO: any validation?
 
-	{	
-		// TODO: time this region and print "bfs,csr,output_file,time_millisec"
-		// TODO: Code for doing BFS on the matrix starting with vertex present in row "root_id"
-		// TODO: Add traversed items into traverse_list
-	}
-	
-	// TODO: store traversal output present in traverse_list into file given by output_file
-	
-	return;
+    IMatrix *mat1;
+    // TODO: Define a List ADT traverse_list to store output.
+
+    // TODO
+    mat1 = load_csr(input_file);
+
+    {
+        // TODO: time this region and print "bfs,csr,output_file,time_millisec"
+        // TODO: Code for doing BFS on the matrix starting with vertex present in row "root_id"
+        // TODO: Add traversed items into traverse_list
+    }
+
+    // TODO: store traversal output present in traverse_list into file given by output_file
+
+    return;
 
 }
 
 
+int main(int n, char *argv[]) {
 
-
-int main(int n, char *argv[])
-{
-
-    if(strcmp("load", argv[1])==0)
-    {
-	load(argv[2], argv[3], argv[4]);
-    }
-    else if( strcmp("add", argv[1])==0)
-    {
-	add(argv[2], argv[3], argv[4], argv[5]);
-    }
-    else if( strcmp("multiply", argv[1])==0 )
-    {
+    if (strcmp("load", argv[1]) == 0) {
+        load(argv[2], argv[3], argv[4]);
+    } else if (strcmp("add", argv[1]) == 0) {
+        add(argv[2], argv[3], argv[4], argv[5]);
+    } else if (strcmp("multiply", argv[1]) == 0) {
         multiply(argv[2], argv[3], argv[4], argv[5]);
-    }
-    else if(strcmp("bfs", argv[1])==0)
-    {
+    } else if (strcmp("bfs", argv[1]) == 0) {
         bfs(argv[2], argv[3], argv[4]);
-    }
-    else 
-        cout<<"[main] invalid input parameters. Valid usage is..."<<endl;
+    } else
+        cout << "[main] invalid input parameters. Valid usage is..." << endl;
 
     return 0;
-    
+
 }
+
+
 
