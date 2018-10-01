2dfda6661ed3b47334a58e8bb4201bb43aab26ae
Shriram Ramesh <shriramrseee@gmail.com>
2018-10-01 11:54:58 +0530

    Implement BFS

--- part_2/code/Runner.cpp
+++ part_2/code/Runner.cpp
@@ -11,6 +11,8 @@
 #include <iomanip>
 #include <fstream>
 #include <ctime>
+#include <list>
+#include <queue>
 #include"MatrixImpl.cpp"
 
 using namespace std;
@@ -18,6 +20,8 @@ using namespace std;
 // Load an input file into an array based matrix
 IMatrix *load_arr(char *input_file) {
     ifstream input;
+    string line, t;
+    stringstream s;
     IMatrix *mat = new ArrayMatrix;
     int N, M;
     // Open the file
@@ -26,24 +30,40 @@ IMatrix *load_arr(char *input_file) {
         throw invalid_argument(string("Input file does not exist!"));
     }
     // Load data into the matrix
-    input >> N >> M;
-    mat->init(N, M);
-    float *row = new float[M];
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            input >> row[j];
-        }
-        mat->append(row);
+    try{
+        getline(input, line);
+        s.str(line);
+        s >> t;
+        N = stoi(t);
+        t = "";
+        s >> t;
+        M = stoi(t);
+        mat->init(N, M);
+        float *row = new float[M];
+        for (int i = 0; i < N; i++) {
+            getline(input, line);
+            stringstream s(line);
+            for (int j = 0; j < M; j++) {
+                t = "";
+                s >> t;
+                row[j] = stof(t);
+            }
+            mat->append(row);
+        }  
+    }
+    catch(const invalid_argument& ia) {
+        throw invalid_argument(string("File has invalid characters or format!"));
     }
     // Close the file
     input.close();
     return mat;
-
 }
 
 // Load an input file into an csr based matrix
 IMatrix *load_csr(char *input_file) {
     ifstream input;
+    string line, t;
+    stringstream s;
     IMatrix *mat = new CSRMatrix;
     int N, M;
     // Open the file
@@ -52,15 +72,31 @@ IMatrix *load_csr(char *input_file) {
         throw invalid_argument(string("Input file does not exist!"));
     }
     // Load data into the matrix
-    input >> N >> M;
-    mat->init(N, M);
-    float *row = new float[M];
-    for (int i = 0; i < N; i++) {
-        for (int j = 0; j < M; j++) {
-            input >> row[j];
-        }
-        mat->append(row);
+    try {
+        getline(input, line);
+        s.str(line);
+        s >> t;
+        N = stoi(t);
+        t = "";
+        s >> t;
+        M = stoi(t);
+        mat->init(N, M);
+        float *row = new float[M];
+        for (int i = 0; i < N; i++) {
+            getline(input, line);
+            stringstream s(line);
+            for (int j = 0; j < M; j++) {
+                t = "";
+                s >> t;
+                row[j] = stof(t);
+            }
+            mat->append(row);
+        }  
     }
+    catch(const invalid_argument& ia) {
+        throw invalid_argument(string("File has invalid characters or format!"));
+    }
+    
     // Close the file
     input.close();
     return mat;
@@ -89,10 +125,12 @@ void print_mat(IMatrix *mat, char *output_file) {
     // Open the file
     output.open(output_file);
     // Write the data to the file
-    output << rows << "\t" << cols << "\t\n";
+    output << rows << "\t" << cols << "\n";
     for (int i = 0; i < rows; i++) {
         for (int j = 0; j < cols; j++) {
-            output << setprecision(3) << mat->get(i, j) << "\t";
+            output << fixed << setprecision(3) << mat->get(i, j);
+            if(j<cols-1)
+                output << "\t";
         }
         output << "\n";
     }
@@ -240,28 +278,79 @@ void multiply(char *mat_type, char *input_file1, char *input_file2, char *output
 }
 
 
-void bfs(char *input_file, char *root_id, char *output_file) {
+// Class to implement BFS depth and node list
+class tl {
 
-    // TODO: any validation?
+public:
+    int depth;
+    list<int> vid;
 
-    IMatrix *mat1;
-    // TODO: Define a List ADT traverse_list to store output.
-
-    // TODO
-    mat1 = load_csr(input_file);
-
-    {
-        // TODO: time this region and print "bfs,csr,output_file,time_millisec"
-        // TODO: Code for doing BFS on the matrix starting with vertex present in row "root_id"
-        // TODO: Add traversed items into traverse_list
+    tl(int d) {
+        depth = d;
     }
 
-    // TODO: store traversal output present in traverse_list into file given by output_file
+};
+
+// Performs BFS on the given graph and stores the result in a file
+void bfs(char *input_file, char *root_id, char *output_file) {
+
+
+    IMatrix *mat1;
+    list<tl> traverse_list;
+    queue<int> q;
+    int nodes, item, root;
+    struct timespec start, end;
+
+    mat1 = load_csr(input_file);
+    nodes = mat1->col_count();
+    int* mat2 = new int[nodes];
+    root=atoi(root_id);
+
+    for(int i=0; i<nodes; i++)
+        mat2[i]=-1;
+
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &start); // Start timer
+    q.push(root);
+    mat2[root]=0;
+    while(!q.empty()) {
+      item = q.front();
+      q.pop();
+      if(traverse_list.size() < mat2[item]+1) {
+          tl *new_depth = new tl(mat2[item]);
+          traverse_list.push_back(*new_depth);
+      }
+      traverse_list.back().vid.push_back(item);
+      for(int i=0; i<nodes; i++) {
+          if(mat1->get(item, i) && mat2[i] == -1) {
+              mat2[i] = mat2[item]+1;
+              q.push(i);
+          }
+      }
+    }
+    clock_gettime(CLOCK_PROCESS_CPUTIME_ID, &end); // End timer
+
+    // Store traversal output present in traverse_list into file given by output_file
+    ofstream output;
+    // Open the file
+    output.open(output_file);
+    // Write the data to the file
+    for (list<tl>::iterator i = traverse_list.begin(); i != traverse_list.end(); i++) {
+        output << i->depth;
+        i->vid.sort();
+        for (list<int>::iterator j = i->vid.begin(); j != i->vid.end(); j++) {
+            output << "," << *j;
+        }
+        output << "\n";
+    }
+    // Close the file
+    output.close();
+
+    // Print time taken in milliseconds to stdout
+    cout << (end.tv_nsec - start.tv_nsec) / 1000000.0 + (end.tv_sec - start.tv_sec) * 1000 << "\n";
 
     return;
 
 }
-
 
 int main(int n, char *argv[]) {
 
